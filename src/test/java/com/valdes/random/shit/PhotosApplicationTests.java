64ba80dc0b831fea1561b8d729b1b50adee56b58
Pablo Valdes <pablovaldes0925@gmail.com>
2024-08-19 22:50:18 -0400

    spring boot practice

---
+++ src/test/java/com/valdes/random/shit/PhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdes.random.shit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
